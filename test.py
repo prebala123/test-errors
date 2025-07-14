dd56e8c1ec893b7de25b9e20a55936b98342bc6d
Pranav Rebala <prebala@ucsd.edu>
2025-07-13 18:07:06 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,6 @@
-user_code = input("Enter code to execute: ")
+# Code that executes a user's mathematical function
+# Example Input: '1 + 1'
+# Output: '2'
+user_code = input("Enter math expression to execute: ")
 # Vulnerable: Evaluating user input as code
 eval(user_code) 
